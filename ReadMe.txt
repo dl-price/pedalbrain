369bcfd1570aa1a0fa170222023ffbd4db4bf4b7
dlprice <daniel.price12@imperial.ac.uk>
2017-01-09 16:31:01 +0000

    Added files

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+
+  pedalbrain
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project pedalbrain
+  Created by Daniel Price on 09/01/2017
+  Copyright © 2017 Daniel Price
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ pedalbrain.ino
@@ -0,0 +1,80 @@
+//
+// pedalbrain
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Daniel Price
+// 				Daniel Price
+//
+// Date			09/01/2017 16:22
+// Version		<#version#>
+//
+// Copyright	© Daniel Price, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
