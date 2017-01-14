87dea84d9b5912fef227b96b0abe7390b18fd349
dlprice <daniel.price12@imperial.ac.uk>
2017-01-14 23:23:00 +0000

    Compiles

--- pedalbrain.ino
+++ pedalbrain.ino
@@ -1,9 +1,13 @@
+#include "includes.h"
+
 void setup() {
   // put your setup code here, to run once:
 
+  PBController::setup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  PBController::loop();
 }
